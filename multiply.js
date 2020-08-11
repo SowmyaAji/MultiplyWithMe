1008f316d369786508d31054231fbdb7bb40056a
Sowmya Aji <sowmya.aji@gmail.com>
2020-08-11 01:37:25 -0400

    Add stats for correct and incorrect answers

--- multiply.js
+++ multiply.js
@@ -6,6 +6,14 @@ let numOne = randomInteger(1, 12);
 let numTwo = randomInteger(1, 12);
 let correctAnswer = numOne * numTwo;
 let correctAnswers = 0;
+let wrongAnswers = 0;
+
+
+function getStats() {
+  // document.getElementById("stats").innerHTML = "Correct answers : " + correctAnswers +  "<br>Wrong answers : " + wrongAnswers + "<br>Total ";
+
+  document.getElementById("stats").innerHTML = "<p>Correct answers :  " + correctAnswers + "</p><p>Wrong answers : " + wrongAnswers + "</p><hr>"
+}
 
 function displayNums() {
   document.getElementById("numbers").innerHTML =
@@ -17,9 +25,11 @@ function multiplyNums() {
   const answer = parseInt(document.getElementById("answer").value);
   if (answer < correctAnswer) {
     result.innerHTML = "That is too low, please enter the answer again.";
+    wrongAnswers += 1;
     document.getElementById("answer").value = "";
   } else if (answer > correctAnswer) {
     result.innerHTML = "That is too high, please enter the answer again.";
+    wrongAnswers += 1
     document.getElementById("answer").value = "";
   } else {
     correctAnswers += 1
@@ -35,3 +45,4 @@ function multiplyAgain() {
   displayNums();
   document.getElementById("answer").value = "";
 }
+
